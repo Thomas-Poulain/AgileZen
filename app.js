0935b210b7f9a44034b983c9fad7d976477382b3
Titouan_J <jamet.e2101129@etud.univ-ubs.fr>
2023-03-22 16:42:13 +0100

    structure du serveur

---
+++ app.js
@@ -0,0 +1,53 @@
+var createError = require('http-errors');
+var express = require('express');
+var session = require('express-session');
+var path = require('path');
+var logger = require('morgan');
+
+
+var indexRouter = require('./routes/index');
+var listUsers = require('./routes/listUsers');
+var connect = require('./routes/connect');
+var disconnect = require('./routes/disconnect');
+var app = express();
+
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(session({
+  secret: 'mysecret',
+  resave: true,
+  saveUninitialized: true,
+  key: null,
+  connected: false
+})) ;
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/listUsers',listUsers);
+app.use('/connect', connect);
+app.use('/disconnect', disconnect);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
