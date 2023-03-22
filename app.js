f56b45ceb1b35915944c189197dc17087c3c0bb0
Titouan_J <jamet.e2101129@etud.univ-ubs.fr>
2023-03-22 18:53:31 +0100

    Version réellement exécutable (juste un 404 tkt)

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 var createError = require('http-errors');
 var express = require('express');
-var mongo = require('mongodb')
 var path = require('path');
 var logger = require('morgan');
 
 
 var indexRouter = require('./routes/index');
-
 var app = express();
 
 
@@ -14,6 +12,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
